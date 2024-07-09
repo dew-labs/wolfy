4c35abf4aa38c9e9e4530911e296de9fc9cf4f49
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2024-07-10 03:40:38 +0700

    ✨ feat(app): add gen abis script

---
+++ scripts/app/getAbis.ts
@@ -0,0 +1,33 @@
+import { json } from "starknet"
+import fs from 'fs'
+
+const CONTRACTS = [
+    ["RoleStore", "./target/dev/satoru_RoleStore.contract_class.json"],
+    ["DataStore", "./target/dev/satoru_DataStore.contract_class.json"],
+    ["EventEmitter", "./target/dev/satoru_EventEmitter.contract_class.json"],
+    ["OracleStore", "./target/dev/satoru_OracleStore.contract_class.json"],
+    ["Oracle", "./target/dev/satoru_Oracle.contract_class.json"],
+    ["OrderVault", "./target/dev/satoru_OrderVault.contract_class.json"],
+    ["IncreaseOrderUtils", "./target/dev/satoru_IncreaseOrderUtils.contract_class.json"],
+    ["DecreaseOrderUtils", "./target/dev/satoru_DecreaseOrderUtils.contract_class.json"],
+    ["SwapOrderUtils", "./target/dev/satoru_SwapOrderUtils.contract_class.json"],
+    ["OrderUtils", "./target/dev/satoru_OrderUtils.contract_class.json"],
+    ["OrderHandler", "./target/dev/satoru_OrderHandler.contract_class.json"],
+    ["DepositVault", "./target/dev/satoru_DepositVault.contract_class.json"],
+    ["DepositHandler", "./target/dev/satoru_DepositHandler.contract_class.json"],
+    ["WithdrawalVault", "./target/dev/satoru_WithdrawalVault.contract_class.json"],
+    ["WithdrawalHandler", "./target/dev/satoru_WithdrawalHandler.contract_class.json"],
+    ["MarketFactory", "./target/dev/satoru_MarketFactory.contract_class.json"],
+    ["Reader", "./target/dev/satoru_Reader.contract_class.json"],
+    ["Router", "./target/dev/satoru_Router.contract_class.json"],
+    ["ExchangeRouter", "./target/dev/satoru_ExchangeRouter.contract_class.json"],
+]
+
+function getAbis() {
+    CONTRACTS.forEach(([name, path]) => {
+        const compiledSierra = json.parse(fs.readFileSync( path).toString( "ascii"))
+        fs.writeFileSync(`${__dirname}/../../artifacts/${name}.abi`, JSON.stringify(compiledSierra.abi),{ flag: 'w' })
+    })
+}
+
+getAbis()
