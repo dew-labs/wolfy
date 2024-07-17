f2245d3f24b238bdb206995356dc197b00093dbe
Huynh Duc Duy <duuuuuuuuy@gmail.com>
2024-07-17 16:21:38 +0700

    ✨ feat(app): update get abis script

--- scripts/app/getAbis.ts
+++ scripts/app/getAbis.ts
@@ -26,7 +26,10 @@ const CONTRACTS = [
 function getAbis() {
     CONTRACTS.forEach(([name, path]) => {
         const compiledSierra = json.parse(fs.readFileSync( path).toString( "ascii"))
-        fs.writeFileSync(`${__dirname}/../../artifacts/${name}.abi`, JSON.stringify(compiledSierra.abi),{ flag: 'w' })
+        const abiContent = JSON.stringify(compiledSierra.abi)
+        fs.writeFileSync(`${__dirname}/../../artifacts/${name}Abi.json`, abiContent,{ flag: 'w' })
+        const tsContent = `const ${name}ABI=${abiContent} as const;export default ${name}ABI`
+        fs.writeFileSync(`${__dirname}/../../artifacts/${name}ABI.ts`, tsContent,{ flag: 'w' })
     })
 }
 
