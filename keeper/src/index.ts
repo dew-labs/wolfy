ba810ecddd99dd09f78c202187a91935c7898328
Anh Tran <yorkittran@gmail.com>
2024-09-06 15:56:51 +0700

    feat(server): deploy to fly & fixes

--- keeper/src/index.ts
+++ keeper/src/index.ts
@@ -1,20 +1,15 @@
-import { json } from "starknet";
 import { logger } from "../../shared/utils/logger";
 import { PriceKeeper } from "./keepers/PriceKeeper";
 import { PythPriceOracleService } from "./services/PythPriceOracleService";
-import { settingUp } from "../../shared/utils/utils";
+import { getTokens, settingUp } from "../../shared/utils/utils";
 
-import fs from "node:fs";
-import setup from "../../shared/utils/setup";
 import type { Token } from "../../shared/interfaces/Token";
 import { OrderKeeper } from "./keepers/OrderKeeper";
 
 async function index() {
-    setup();
+    const { account, chainId, hermesUrl } = await settingUp();
 
-    const { net, account, chainId, hermesUrl } = await settingUp();
-
-    const tokens: Token[] = json.parse(fs.readFileSync(`./tokens.${net}.json`).toString("ascii"));
+    const tokens: Token[] = getTokens();
 
     const priceOracleService = new PythPriceOracleService(hermesUrl, tokens);
 
