e6ab5562f675f387886339892cc3ab1dfb443f10
baokhanh13 <khanh.b@raksul.com>
2024-10-16 15:28:19 +0700

    ✨ feat(indexer): Index action
    Changelog: feature

--- apps/indexer/.checkpoint/models.ts
+++ apps/indexer/.checkpoint/models.ts
@@ -8,6 +8,7 @@ export class Order extends Model {
 
     this.initialSet('id', id);
     this.initialSet('account', "");
+    this.initialSet('action', "");
     this.initialSet('key', "");
     this.initialSet('market', "");
     this.initialSet('order_type', "");
@@ -53,6 +54,14 @@ export class Order extends Model {
 
   set account(value: string) {
     this.set('account', value);
+  }
+
+  get action(): string {
+    return this.get('action');
+  }
+
+  set action(value: string) {
+    this.set('action', value);
   }
 
   get key(): string {
