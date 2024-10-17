c1b66dd10901342f18cf6cc795b50ec96dd61706
baokhanh13 <khanh.b@raksul.com>
2024-10-17 22:11:51 +0700

    🔨 refactor(backend): Port config
    Changelog: refactor

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -16,6 +16,6 @@ const app = new Elysia()
         })
     )
     .group("/api/v1", (app) => app.use(accountRoute))
-    .listen(config.BACKEND_PORT || 3000);
+    .listen(config.BACKEND_PORT || 3002);
 
 console.log(`🦊 Backend is running at ${app.server?.hostname}:${app.server?.port}`);
