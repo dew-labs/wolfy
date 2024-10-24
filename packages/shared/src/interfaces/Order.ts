eea3f599f9a07c704b4130c70951ef94442456c8
Anh Tran <yorkittran@gmail.com>
2024-10-24 17:39:43 +0700

    🐛 fix(keeper): not store index token address
    Changelog: fix

--- packages/shared/src/interfaces/Order.ts
+++ packages/shared/src/interfaces/Order.ts
@@ -6,21 +6,7 @@ export interface Order {
     market: string;
     orderType: OrderType;
     isLong: boolean;
-    indexTokenAddress: string;
     sizeDeltaUsd: bigint;
     triggerPrice?: bigint;
     acceptablePrice: bigint;
 }
-
-export const OrderSchema = Type.Object({
-    key: Type.String(),
-    market: Type.String(),
-    orderType: Type.Enum(OrderType),
-    isLong: Type.Boolean(),
-    indexTokenAddress: Type.String(),
-    sizeDeltaUsd: Type.BigInt(),
-    triggerPrice: Type.Optional(Type.BigInt()),
-    acceptablePrice: Type.BigInt(),
-});
-
-export const OrdersSchema = Type.Optional(Type.Record(Type.String(), Type.Array(OrderSchema)));
